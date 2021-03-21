7594977af0682244f07999f10311e278a3d59d42
risaddex <risaddex@gmail.com>
2021-03-20 22:52:00 -0300

    pre start

--- app.ts
+++ app.ts
@@ -1,39 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+import express from "express"
+import * as http from "http"
+import cors from "cors"
+import debug from "debug"
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app: express.Application = express()
+const server: http.Server = http.createServer(app)
+const port = 3000
+const debugLog: debug.IDebugger = debug("app")
 
-var app = express();
+// here we are adding middleware to parse all incoming requests as JSON
+app.use(express.json())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// here we are adding middleware to allow cross-origin requests
+app.use(cors())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+// this is a simple route to make sure everything is working properly
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).send(`Server up and running!`)
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+server.listen(port, () => {
+  debugLog(`Server running at http://localhost:${port}`)
+})
