37e4a66331c8ae3b102d5e87f15bd7135412b32f
risaddex <risaddex@gmail.com>
2021-03-20 23:06:01 -0300

    pre config

--- app.ts
+++ app.ts
@@ -1,24 +1,23 @@
-import express from "express"
-import * as http from "http"
-import cors from "cors"
-import debug from "debug"
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
+import express from 'express';
+import * as http from 'http';
+import cors from 'cors';
+import debug from 'debug';
 
-const app: express.Application = express()
-const server: http.Server = http.createServer(app)
-const port = 3000
-const debugLog: debug.IDebugger = debug("app")
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = 3000;
+const debugLog: debug.IDebugger = debug('app');
 
 // here we are adding middleware to parse all incoming requests as JSON
-app.use(express.json())
+app.use(express.json());
 
 // here we are adding middleware to allow cross-origin requests
-app.use(cors())
-
-// this is a simple route to make sure everything is working properly
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.status(200).send(`Server up and running!`)
-})
+app.use(cors());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 server.listen(port, () => {
-  debugLog(`Server running at http://localhost:${port}`)
-})
+  debugLog(`Server running at http://localhost:${port}`);
+});
